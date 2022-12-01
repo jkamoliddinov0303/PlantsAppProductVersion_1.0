7e56c37b826a45d0e5b60152f726b75d3b2b62e8
Jamoliddin <https://github.com/jkamoliddinov0303/NewSmartHouseApp2>
2022-12-01 09:38:01 +0500

    Initial commit

---
+++ app/src/main/java/uz/jkamoliddinov/plantsappproductversion_10/ui/activities/IntroActivity.kt
@@ -0,0 +1,12 @@
+package uz.jkamoliddinov.plantsappproductversion_10.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import uz.jkamoliddinov.plantsappproductversion_10.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
---
+++ app/src/main/java/uz/jkamoliddinov/plantsappproductversion_10/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package uz.jkamoliddinov.plantsappproductversion_10.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.jkamoliddinov.plantsappproductversion_10.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
